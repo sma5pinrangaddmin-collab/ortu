268483d0575480524192d90b4a384babe21f56a5
sma5pinrangaddmin-collab <sma5pinrangaddmin@gmail.com>
2026-07-08 22:25:12 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.GITHUB_CONFIG = {
   
-  gasUrl: "https://script.google.com/macros/s/AKfycbwVv_BtiKhpO48f2jQ8_WYKBRa0PT8Lf5dHFvt7oi8qnkX5HKZd5vDGBwO4bhoSC9q5/exec",   //pass    
+  gasUrl: "https://script.google.com/macros/s/AKfycbw15flybmF_YYCnIPAinC68A4U84eNkItNTIga80Dl-AaHHU7MPfi2iubf_-Y9uQeXP/exec",   //pass    
 };
